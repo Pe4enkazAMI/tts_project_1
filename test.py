ad6297fac5c8374d0bf644a7e3fdde8914452b16
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-11-20 18:50:55 +0300

    ya ebal

--- test.py
+++ test.py
@@ -70,6 +70,7 @@ def main(config, out_file):
     i = 0
     for audio in audios:
         path_to_save = Path(f"/kaggle/working/{i}.wav")
+        print("SHAPE:", audio.shape)
         torchaudio.save(path_to_save, audio.squeeze(0), sample_rate=22050)
             
         
