bb3027d31b49e0a4203452894b93ffb06518e68e
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-11-20 18:05:37 +0300

    jopa

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ def main(config, out_file):
     i = 0
     for audio in audios:
         path_to_save = Path(f"/kaggle/working/{i}.wav")
-        torchaudio.save(path_to_save, audio)
+        torchaudio.save(path_to_save, audio, sample_rate=22050)
             
         
 
