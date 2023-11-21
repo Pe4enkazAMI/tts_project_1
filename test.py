10da19c84d0daee580dae82a831ed2f9a233d294
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-11-21 15:07:07 +0300

    test

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ def main(config, out_file):
     WaveGlow.to(device)
 
     testing = [ 
-        "I love Sofia Kostina to the moon and back"
+        "I love Sofia Kostina to the moon and back",
         "A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest",
         "Massachusetts Institute of Technology may be best known for its math, science and engineering education",
         "Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space",
