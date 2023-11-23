f0dff7a0d7b97d25449ff07115852b3935f22a42
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-11-23 13:39:36 +0500

    maploc

--- hw_tts/utils/util.py
+++ hw_tts/utils/util.py
@@ -204,7 +204,7 @@ def get_WaveGlow():
     waveglow_path = os.path.join("waveglownet", "pretrained_model")
     waveglow_path = os.path.join("/kaggle/input/fastspeecch-dataset",
                                   "waveglow_256channels.pt")
-    wave_glow = torch.load(waveglow_path)['model']
+    wave_glow = torch.load(waveglow_path, map_location="cpu")['model']
     wave_glow = wave_glow.remove_weightnorm(wave_glow)
     wave_glow.cuda().eval()
     for m in wave_glow.modules():
